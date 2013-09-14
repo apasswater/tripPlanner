7c7fd488aa11e5454c0ef0e057d951e200e88ca5
Andrea <apasswater@gmail.com>
2013-09-14 16:15:24 -0700

    Added menu
    Only Create new works.

--- tripPlanner.py
+++ tripPlanner.py
@@ -1,23 +1,47 @@
 trip_dict = {
-	"Tahoe":"Jess"
+	"Ski trip": {"Tahoe": ["Jess"]},
+	"Weekend in Seattle": {"Seattle": ["Katie"]},
+	"Yoga in India": {"Mumbai": ["Sahid"]},
+	"Trip to Chile": {"Santiago": ["Monica"]}
 }
+
+'''Get username for appending to dictionary later'''
 print "Welcome to tripPlanner!"
 username = raw_input('What is your name? ')
 username = username.capitalize()
 
-'''Loop indefinitely until user confirm is yes'''
-while True:
-	destination = raw_input('Hey ' + username + '! Where would you like to go? ')
-
-	'''This will capitalize the first letter of each word in user input'''
-	destination = ' '.join(word[0].upper() + word[1:] for word in destination.split())
-
-	confirm = raw_input('You would like to go to ' + destination + '. Is that correct? Y/N: ')
-	confirm = confirm.upper()
-	if confirm == "Y":
-		break
+'''Be nice and say hello'''
+print "\nHey " + username + "!"
+		
+'''Check if the city the user typed in is already an existing trip'''
+def does_trip_exist(destination):
+	extisting_trip = False
+	for i in trip_dict:
+		if trip_dict[i].has_key(destination):
+			friend = trip_dict[i].get(destination)[0]
+			existing_trip = True
+	if existing_trip == True:
+		print "It looks like " + str(friend) + " wants to go to " + destination + "!"
+		join_trip()
 	else:
-		print "My bad. Let's try this again. "
+		print "It looks like none of your friends have started a trip to " + destination + "." 
+		create_new()
+
+'''Asks user where they would like to go'''
+def ask_destination():
+	while True:
+		destination = raw_input('Which city would you like to go to? ')
+
+		'''This will capitalize the first letter of each word in user input'''
+		destination = ' '.join(word[0].upper() + word[1:] for word in destination.split())
+
+		confirm = raw_input('You would like to go to ' + destination + '. Is that correct? Y/N: ')
+		confirm = confirm.upper()
+		if confirm == "Y":
+			does_trip_exist(destination)
+			break
+		else:
+			print "My bad. Let's try this again. "
 
 '''Function for user joining a trip'''
 def join_trip():
@@ -30,22 +54,39 @@ def join_trip():
 
 '''Function to create a new trip'''
 def create_new():
-	new_trip = raw_input('Create a new trip? ')
+	new_trip = raw_input('Create a new trip? Y/N ')
 	new_trip = new_trip.upper()
 	if new_trip == 'Y':
 		print "Ok, let's get started!"
+		### need to create a new trip here ###
 	else:
-		print "Ok, come back any time to check other trips!"
+		initial_selection()
 
-# Check to see if the trip already exists
-# User can either join an existing trip or create a new one
-def does_trip_exist():
-	if trip_dict.has_key(destination):
-		friend = trip_dict.get(destination)
-		print "It looks like " + friend + " wants to go to " + destination + "!"
-		join_trip()
-	else:
-		print "It looks like none of your friends have started a trip to " + destination + "." 
-		create_new()
 
-does_trip_exist()
+'''Present menu, and let user make a selection'''
+def initial_selection():
+	while True:
+		###
+		print "\n"
+		print "*** MENU ***"
+		print "(C)reate a new trip"
+		print "(V)iew your friends' trips"
+		print "(E)xit"                     #Exit doesn't work yet 
+		print "\n"
+		###
+
+		menu_choice = raw_input('What would you like to do? ')
+		menu_choice = menu_choice.upper()
+
+		if (menu_choice != "C") and (menu_choice != "V") and (menu_choice != "E"):
+			print "Sorry, that's not a valid selection."
+		else:
+			if menu_choice == "C":
+				ask_destination()
+			elif menu_choice == "V":
+				print trip_dict
+			else:
+				print "Ok, see you next time!"
+			break
+
+initial_selection()
